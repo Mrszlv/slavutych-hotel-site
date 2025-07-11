24953afd420125f0bdcabb84ea0b20e36caecbb6
Miroszlav <miroszlav.popovics@gmail.com>
2025-07-11 19:49:35 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { useTranslation } from "react-i18next";
 
 import Header from "./components/Header/Header";
-import LanguageSwitcher from "./components/LanguageSwitcher/LanguageSwitcher";
-import Navigation from "./components/Navigation/Navigation";
 import HomePage from "./pages/HomePage/HomePage";
 import Rooms from "./pages/Rooms/Rooms";
 import Gallery from "./pages/Gallery/Gallery";
 import Reviews from "./pages/Reviews/Reviews";
 import Contact from "./pages/Contact/Contact";
-
-import Hero from "./components/hero/hero";
 import Footer from "./components/Footer/Footer";
 
 function App() {
-  const { t } = useTranslation();
   return (
     <Router>
       <div>
