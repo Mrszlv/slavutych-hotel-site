a0f7e3f303d16a890cd6fb50a78ae812ac41d8a9
Miroszlav <miroszlav.popovics@gmail.com>
2025-07-16 20:26:45 +0300

    contacts

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -9,18 +9,21 @@ const Contact = () => {
 
   return (
     <div className={`${s.container} ${s.photoCont}`}>
-      <h2 className={s.title}>{t("contact.title")}</h2>
+      <h2 className={s.title}>
+        {t("contact.title")}{" "}
+        <span className={s.subtitle}>{t("contact.subtitle")}</span>{" "}
+      </h2>
+      <h3 className={s.contactsText}>{t("contact.text")}</h3>
       <address className={s.contactsAddress}>
         <ul className={s.contactsList}>
           <li className={s.contactsItem}>
-            <p className={s.contactsText}>{t("contact.text")}</p>
-            <a href="tel:+380951379410" className={s.addText}>
-              <span className={s.span}>Vodafone:</span> +380 (95) 137-94-10
+            <a href="tel:+380958065331" className={s.addText}>
+              Vodafone: +380 (95) 806-53-31
             </a>
           </li>
           <li className={s.contactsItem}>
-            <a href="tel:+380680660303" className={s.addText}>
-              <span className={s.span}>Kyivstar:</span> +380 (68) 066-03-03
+            <a href="tel:+380954728246" className={s.addText}>
+              Kyivstar: +380 (95) 472-82-46
             </a>
           </li>
           <li className={s.contactsItem}>
@@ -28,11 +31,10 @@ const Contact = () => {
               href="mailto:slavutych.zakarpattia@gmail.com"
               className={s.addText}
             >
-              <span className={s.span}>Email:</span>{" "}
-              slavutych.zakarpattia@gmail.com
+              Email: slavutych.zakarpattia@gmail.com
             </a>
           </li>
-          <li className={s.contactsItem}>
+          {/* <li className={s.contactsItem}>
             <p className={s.contactsText}>{t("contact.manager")}</p>
             <a href="tel:+380951379410" className={s.addText}>
               <span className={s.span}>Vodafone:</span> +380 (50) 681-54-36
@@ -42,10 +44,13 @@ const Contact = () => {
             <a href="tel:+380680660303" className={s.addText}>
               <span className={s.span}>Kyivstar:</span> +380 (97) 338-58-94
             </a>
-          </li>
+          </li> */}
         </ul>
       </address>
-      <h2 className={s.title}>{t("contact.route")}</h2>
+      <h2 className={s.title}>
+        {t("contact.route")}{" "}
+        <span className={s.subroute}>{t("contact.subroute")} </span>{" "}
+      </h2>
       <h3 className={s.title}>{t("contact.automobile")}</h3>
       <p className={s.text}>{t("contact.autoDescr")}</p>
       <h3 className={s.title}>{t("contact.bus")}</h3>
