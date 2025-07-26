4f9c82bee62185990c6a5f1cf6aec1fd871d3070
Miroszlav <miroszlav.popovics@gmail.com>
2025-07-26 15:49:44 +0300

    booking-form

--- src/components/BookingForm/BookingForm.jsx
+++ src/components/BookingForm/BookingForm.jsx
@@ -1,70 +1,94 @@
-import { useForm } from "react-hook-form";
-
 import { useTranslation } from "react-i18next";
+import iziToast from "izitoast";
+import emailjs from "@emailjs/browser";
+import { useRef } from "react";
 
 import s from "./BookingForm.module.css";
 
 const BookingForm = () => {
   const { t } = useTranslation();
+  const form = useRef();
 
-  const { register, handleSubmit } = useForm();
-
-  const onSubmit = (data) => {
-    console.log("Form submitted:", data);
-    alert(t("Success"));
+  const onSubmit = (e) => {
+    e.preventDefault();
+    emailjs
+      .sendForm("service_tglaf38", "template_gtmgywa", form.current, {
+        publicKey: "5HJiJxVQzx39CK539",
+      })
+      .then(
+        () => {
+          iziToast.success({
+            title: t("footer.iziToastSuccessTitle"),
+            message: t("footer.iziToastSuccessMessage"),
+            position: "topCenter",
+          });
+          form.current.reset();
+        },
+        (error) => {
+          iziToast.error({
+            title: t("footer.iziToastErrorTitle"),
+            message: t("footer.iziToastErrorMessage"),
+            position: "topCenter",
+          });
+          console.error("Email error:", error.text);
+        }
+      );
   };
+
   return (
-    <>
+    <div className={s.container}>
       <h3 className={s.title}>
         {t("bookingForm.title")}
         <span className={s.subtitle}>{t("bookingForm.subtitle")}</span>
       </h3>
+
       <div className={s.bookingFormCont}>
         <img
           className={s.img}
-          src="https://res.cloudinary.com/ddqtzq55a/image/upload/v1738924893/IMG_2479_vatdmv.jpg"
-          alt=""
+          src="https://res.cloudinary.com/ddqtzq55a/image/upload/v1753099424/IMG_2479_vatdmv_vcsbsj.jpg"
+          alt="Booking"
         />
 
-        <form className={s.form} onSubmit={handleSubmit(onSubmit)}>
+        <form ref={form} className={s.form} onSubmit={onSubmit}>
           <input
             className={s.input}
-            {...register("name")}
+            type="text"
+            name="user_name"
             placeholder={t("bookingForm.name")}
             required
           />
+
           <input
             className={s.input}
-            type="number"
-            {...register("number")}
+            type="tel"
+            name="user_number"
             placeholder={t("bookingForm.number")}
             required
           />
+
           <input
             className={s.input}
             type="number"
-            {...register("people")}
+            name="user_people"
             placeholder={t("bookingForm.people")}
             required
           />
+
+          <input className={s.input} type="date" name="user_checkIn" required />
+
           <input
             className={s.input}
             type="date"
-            {...register("checkIn")}
+            name="user_checkOut"
             required
           />
-          <input
-            className={s.input}
-            type="date"
-            {...register("checkOut")}
-            required
-          />
+
           <button className={s.btn} type="submit">
             {t("bookingForm.book")}
           </button>
         </form>
       </div>
-    </>
+    </div>
   );
 };
 
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,10 +1,12 @@
 import Hero from "../../components/Hero/Hero";
 import Services from "../../components/Services/Services";
+import BookingForm from "../../components/BookingForm/BookingForm";
 
 const HomePage = () => {
   return (
     <div>
       <Hero />
+      <BookingForm />
       <Services />
     </div>
   );
